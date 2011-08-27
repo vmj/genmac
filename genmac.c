5597346fa5ea740798aa42eae2e6c99a4787373e
Mikko Värri <vmj@linuxbox.fi>
2011-08-27 03:14:46 +0300

    Version 0.2

--- genmac.c
+++ genmac.c
@@ -8,7 +8,7 @@
 
 // See info libc -> Argp Global Variables
 
-const char * argp_program_version = "0.1";
+const char * argp_program_version = "0.2";
 const char * argp_program_bug_address = "vmj@linuxbox.fi";
 
 // Local types
