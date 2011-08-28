8b40b4880e93e332fe19c12c555abbd3253a3f20
Mikko Värri <vmj@linuxbox.fi>
2011-08-29 00:24:19 +0300

    Add copyright notice and a statement of copying permission.

--- genmac.c
+++ genmac.c
@@ -1,4 +1,21 @@
-
+/*
+ * genmac - Generate a random MAC address.
+ *
+ * Copyright (C) 2011 Mikko Värri (vmj@linuxbox.fi)
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include <sys/types.h> // for pid_t
 #include <unistd.h>    // for getpid()
 #include <stdint.h>    // for uint8_t
